009a19727ef50d0a17dda3064faf4701d74ed00d
Enki Ferreira Costa <65816689+RetroGamerCX@users.noreply.github.com>
2020-09-20 12:09:20 -0300

    Update simple macOS installer.sh

--- simple macOS installer.sh
+++ simple macOS installer.sh
@@ -10,3 +10,7 @@ sudo apt upgrade --yes
 sudo apt install qemu-kvm qemu-system-x86 qemu-img --yes
 wget https://github.com/RetroGamerCX/simple-macOS/blob/master/snap/local/ESP.qcow2?raw=true
 mv 'ESP.qcow2?raw=true' ESP.qcow2
+sudo apt install dmg2img --yes
+dmg2img -v -i BaseSystem.dmg BaseSystem.img
+wget https://raw.githubusercontent.com/RetroGamerCX/simple-macOS/master/snap/local/bin/launch
+sudo chmod +x launch
