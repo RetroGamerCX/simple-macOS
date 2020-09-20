9d9eab1fbfdff086abdbacb67a1d6d69a3b28341
Enki Ferreira Costa <65816689+RetroGamerCX@users.noreply.github.com>
2020-09-20 14:27:35 -0300

    Update simple macOS installer.sh

--- simple macOS installer.sh
+++ simple macOS installer.sh
@@ -8,6 +8,13 @@ sudo chmod +x fetch-macOS.py
 sudo apt update
 sudo apt upgrade --yes
 sudo apt install qemu-kvm qemu-system-x86 qemu-img --yes
+mkdir ~/simple-macOS/firmware
+cd ~/simple-macOS/irmware
+wget https://github.com/RetroGamerCX/simple-macOS/blob/master/snap/local/firmware/OVMF_CODE.fd?raw=true
+mv 'OVMF_CODE.fd?raw=true' 'OVMF_CODE.fd'
+wget https://github.com/RetroGamerCX/simple-macOS/blob/master/snap/local/firmware/OVMF_VARS-1024x768.fd?raw=true
+mv 'OVMF_VARS-1024x768.fd?raw=true' OVMF_VARS-1024x768.fd
+cd ~/simple-macOS
 wget https://github.com/RetroGamerCX/simple-macOS/blob/master/snap/local/ESP.qcow2?raw=true
 mv 'ESP.qcow2?raw=true' ESP.qcow2
 sudo apt install dmg2img --yes
